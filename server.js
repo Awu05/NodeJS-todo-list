c716f146eaa62c7592e8e57979bfd40d2ca62730
Andy Wu <andywu.05@gmail.com>
2020-07-26 17:53:24 -0400

    Working rest server

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.port || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/todoListModel'),  //created model Loading here
+    bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');  //import route
+routes(app);   //register the route
+
+//Middleware must go at the end of routes(app)
+//This uses express to intercept incoming http requests
+app.use(function(req, res) {
+    res.status(400).send({url: req.originalUrl + ' not found'})
+});
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on port: ' + port);
