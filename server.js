d1541c3625cea791246e336782067b0b85df1c0e
Andy Wu <andywu.05@gmail.com>
2020-07-26 21:28:08 -0400

    Configed mongo to use heroku and added dotenv lib to get env vars

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.port || 3000,
-    mongoose = require('mongoose'),
-    Task = require('./api/models/todoListModel'),  //created model Loading here
-    bodyParser = require('body-parser');
+var express = require("express"),
+  app = express(),
+  port = process.env.port || 3000,
+  mongoose = require("mongoose"),
+  Task = require("./api/models/todoListModel"), //created model Loading here
+  bodyParser = require("body-parser");
+require("dotenv").config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
+// mongoose.connect('mongodb://localhost/Tododb');
+// connect Mongoose to your DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/todolist-test12"
+);
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/todoListRoutes');  //import route
-routes(app);   //register the route
+var routes = require("./api/routes/todoListRoutes"); //import route
+routes(app); //register the route
 
 //Middleware must go at the end of routes(app)
 //This uses express to intercept incoming http requests
-app.use(function(req, res) {
-    res.status(400).send({url: req.originalUrl + ' not found'})
+app.use(function (req, res) {
+  res.status(400).send({ url: req.originalUrl + " not found" });
 });
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on port: ' + port);
+console.log("todo list RESTful API server started on port: " + port);
